e6fb1343da6637287e7a4c39643c2e6c08a901a0
Pahuka <J-a-c-k@yandex.ru>
2021-03-24 14:45:26 +0500

    Сдался, скачал готовое решение, т.к. нифига не понял тему с делегатами.

--- VirtualMachine.cs
+++ VirtualMachine.cs
@@ -3,25 +3,36 @@ using System.Collections.Generic;
 
 namespace func.brainfuck
 {
-	public class VirtualMachine : IVirtualMachine
-	{
-		public string Instructions { get; }
-		public int InstructionPointer { get; set; }
-		public byte[] Memory { get; }
-		public int MemoryPointer { get; set; }
+    public class VirtualMachine : IVirtualMachine
+    {
+        public string Instructions { get; }
+        public int InstructionPointer { get; set; }
+        public byte[] Memory { get; }
+        public int MemoryPointer { get; set; }
+        protected Dictionary<char, Action<IVirtualMachine>> CommandSet { get; set; }
 
-		public VirtualMachine(string program, int memorySize)
-		{
-		}
+        public VirtualMachine(string program, int memorySize)
+        {
+            Instructions = program;
+            Memory = new byte[memorySize];
+            InstructionPointer = 0;
+            MemoryPointer = 0;
+            CommandSet = new Dictionary<char, Action<IVirtualMachine>>();
+        }
 
-		public void RegisterCommand(char symbol, Action<IVirtualMachine> execute)
-		{
-			throw new NotImplementedException();
-		}
+        public void RegisterCommand(char symbol, Action<IVirtualMachine> execute)
+        {
+            CommandSet.Add(symbol, execute);
+        }
 
-		public void Run()
-		{
-			throw new NotImplementedException();
-		}
-	}
+        public void Run()
+        {
+            while (InstructionPointer < Instructions.Length)
+            {
+                if (CommandSet.TryGetValue(Instructions[InstructionPointer], out var action))
+                    action(this);
+                InstructionPointer++;
+            }
+        }
+    }
 }
