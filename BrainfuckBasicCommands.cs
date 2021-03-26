3ab74d8afa91a12cebc740bbb6f222f4db249735
Pahuka <J-a-c-k@yandex.ru>
2021-03-26 16:07:26 +0500

    Реализовал все задачи в классе BrainfuckBasicCommands

--- BrainfuckBasicCommands.cs
+++ BrainfuckBasicCommands.cs
@@ -21,11 +21,21 @@ namespace func.brainfuck
             });
             vm.RegisterCommand('+', b => 
             {
-                b.Memory[b.MemoryPointer]++;
+                b.Memory[b.MemoryPointer] = (byte)((b.Memory[b.MemoryPointer] + 1) % 256);
             });
-            vm.RegisterCommand('-', b => { b.Memory[b.MemoryPointer]--; });
-            vm.RegisterCommand('>', b => { b.MemoryPointer++; });
-            vm.RegisterCommand('<', b => { b.MemoryPointer--; });
+            vm.RegisterCommand('-', b => 
+            {
+                var temValue = b.Memory[b.MemoryPointer];
+                b.Memory[b.MemoryPointer] = temValue <= 0 ? (byte)255 : temValue -= 1;
+            });
+            vm.RegisterCommand('>', b => 
+            {
+                b.MemoryPointer = (b.MemoryPointer + 1) % b.Memory.Length;
+            });
+            vm.RegisterCommand('<', b => 
+            {
+                b.MemoryPointer = b.MemoryPointer <= 0 ? b.Memory.Length - 1 : b.MemoryPointer -= 1 ;
+            });
             vm.RegisterCommand(',', b =>
                 {
                     var x = read.Invoke();
