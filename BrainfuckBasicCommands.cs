fd3ce49365ca0983187d5f69101913dc4ba3a0fd
Pahuka <J-a-c-k@yandex.ru>
2021-03-26 14:07:35 +0500

    Реализовал смещение по памяти, ввод по регулярным выражениям.

--- BrainfuckBasicCommands.cs
+++ BrainfuckBasicCommands.cs
@@ -8,11 +8,7 @@ namespace func.brainfuck
     public class BrainfuckBasicCommands
     {
         public static void RegisterTo(IVirtualMachine vm, Func<int> read, Action<char> write)
-        {            
-            Encoding encoding = Encoding.ASCII;
-            var c = encoding.GetBytes(vm.Instructions);
-            var a = (char)c[0];
-
+        {
             foreach (var item in vm.Instructions)
             {
                 if (char.IsLetterOrDigit(item))
@@ -23,10 +19,13 @@ namespace func.brainfuck
             {
                write.Invoke((char)b.Memory[b.MemoryPointer]);
             });
-            vm.RegisterCommand('+', b => { });
-            vm.RegisterCommand('-', b => { });
-            vm.RegisterCommand('>', b => { });
-            vm.RegisterCommand('<', b => { });
+            vm.RegisterCommand('+', b => 
+            {
+                b.Memory[b.MemoryPointer]++;
+            });
+            vm.RegisterCommand('-', b => { b.Memory[b.MemoryPointer]--; });
+            vm.RegisterCommand('>', b => { b.MemoryPointer++; });
+            vm.RegisterCommand('<', b => { b.MemoryPointer--; });
             vm.RegisterCommand(',', b =>
                 {
                     var x = read.Invoke();
