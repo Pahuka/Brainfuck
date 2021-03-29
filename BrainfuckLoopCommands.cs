ffdd06b554847083cba6bacda3a9e459232c03bd
Pahuka <J-a-c-k@yandex.ru>
2021-03-29 11:29:56 +0500

    Реализовал BrainfuckLoopCommands через два противоположных друг другу словаря.

--- BrainfuckLoopCommands.cs
+++ BrainfuckLoopCommands.cs
@@ -6,8 +6,36 @@ namespace func.brainfuck
 	{
 		public static void RegisterTo(IVirtualMachine vm)
 		{
-			vm.RegisterCommand('[', b => { });
-			vm.RegisterCommand(']', b => { });
+            List<int> startLoop = new List<int>();
+            List<int> endLoop = new List<int>();
+            Dictionary<int, int> startProgLp = new Dictionary<int, int>();
+            Dictionary<int, int> endProgLp = new Dictionary<int, int>();
+
+            for (int i = 0; i < vm.Instructions.Length; i++)
+            {
+                if (vm.Instructions[i] == '[') startLoop.Add(i);
+                if (vm.Instructions[i] == ']') endLoop.Add(i);
+            }
+            endLoop.Reverse();
+
+            for (int i = 0; i < startLoop.Count; i++)
+            {
+                startProgLp.Add(startLoop[i], endLoop[i]);
+            }
+
+            foreach (var item in startProgLp)
+            {
+                endProgLp.Add(item.Value, item.Key);
+            }
+
+			vm.RegisterCommand('[', b => {
+                if (b.Memory[b.MemoryPointer] == 0)
+                    b.InstructionPointer = startProgLp[b.InstructionPointer];
+            });
+			vm.RegisterCommand(']', b => {
+                if (b.Memory[b.MemoryPointer] != 0)
+                    b.InstructionPointer = endProgLp[b.InstructionPointer];
+            });
 		}
 	}
 }
