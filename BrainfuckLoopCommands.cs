afb9daeb8cbbee994f301ad2bd73b08167b139e7
Pahuka <J-a-c-k@yandex.ru>
2021-03-29 18:00:50 +0500

    Полностью завершил задачу.

--- BrainfuckLoopCommands.cs
+++ BrainfuckLoopCommands.cs
@@ -6,29 +6,18 @@ namespace func.brainfuck
 	{
 		public static void RegisterTo(IVirtualMachine vm)
 		{
-            List<int> startLoop = new List<int>();
-            List<int> endLoop = new List<int>();
             Dictionary<int, int> startProgLp = new Dictionary<int, int>();
             Dictionary<int, int> endProgLp = new Dictionary<int, int>();
+            Stack<int> list = new Stack<int>();
 
-            for (int i = 0; i < vm.Instructions.Length; i++)
-            {
-                if (vm.Instructions[i] == '[') startLoop.Add(i);
-                if (vm.Instructions[i] == ']') endLoop.Add(i);
-            }
-            endLoop.Reverse();
-
-            for (int i = 0; i < startLoop.Count; i++)
-            {
-                startProgLp.Add(startLoop[i], endLoop[i]);
-            }
+            LoopProcces(vm, startProgLp, list);
 
             foreach (var item in startProgLp)
             {
                 endProgLp.Add(item.Value, item.Key);
             }
 
-			vm.RegisterCommand('[', b => {
+            vm.RegisterCommand('[', b => {
                 if (b.Memory[b.MemoryPointer] == 0)
                     b.InstructionPointer = startProgLp[b.InstructionPointer];
             });
@@ -37,5 +26,34 @@ namespace func.brainfuck
                     b.InstructionPointer = endProgLp[b.InstructionPointer];
             });
 		}
+
+        public static void LoopProcces(IVirtualMachine vm, Dictionary<int, int> startProgLp, Stack<int> list)
+        {
+            for (int i = 0; i < vm.Instructions.Length; i++)
+            {
+                if (vm.Instructions[i] == '[')
+                {
+                    for (int j = 1 + i; j < vm.Instructions.Length; j++)
+                    {
+                        if (vm.Instructions[j] == ']')
+                        {
+                            startProgLp.Add(i, j);
+                            i = 1 + j;
+                            break;
+                        }
+                        if (vm.Instructions[j] == '[')
+                        {
+                            list.Push(i);
+                            break;
+                        }
+                    }
+                }
+
+                if (vm.Instructions[i] == ']')
+                {
+                    startProgLp.Add(list.Pop(), i);
+                }
+            }
+        }
 	}
 }
